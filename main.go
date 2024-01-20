bbe09b59b9b720a8a2c141144ee7d24081c8cc59
semih babacan huawei <m.semihbabacan@gmail.com>
2024-01-20 06:12:10 +0300

    Add: added some code

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type bencodeInfo struct {
+	Pieces      string `bencode:"pieces"`
+	PieceLength int    `bencode:"piece length"`
+	Length      int    `bencode`
+}
+
 func main() {
 	fmt.Println("Starting the client...")
 
